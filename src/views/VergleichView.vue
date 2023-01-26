15a64754d512fef3167abda28c35e07562cf70bf
Daniel Ackermann <d.ackermann@smf.de>
2023-01-26 10:17:03 +0100

    Neu angeordnet und die Auswahlmenüs als Drawer umgesetzt

--- src/views/VergleichView.vue
+++ src/views/VergleichView.vue
@@ -1,76 +1,101 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="1">
-        <v-btn>Alle aktivieren</v-btn>
-        <v-checkbox v-for="bucket in buckets" :key="bucket.id" v-model="selected" :value="bucket.id"
-                    :label=bucket.name @change="sortSelectedBuckets">
-        </v-checkbox>
-      </v-col>
-      <v-col class="d-flex justify-center align-center">
-        <v-btn
-            class="ma-2"
-            color="primary"
-            dark
+  <v-container fluid class="fill-height">
+    <v-row class="fill-height">
+      <v-col align-self="center" class="justify-start" cols="1">
+<!--        <v-btn
+            color="pink"
+            @click.stop="drawer = !drawer"
         >
-          Accept
-          <v-icon
-              dark
-              right
-          >
-            mdi-checkbox-marked-circle
-          </v-icon>
-        </v-btn>
-        <v-simple-table style="width: 30%;">
-          <tr>
-            <th>ID</th>
-            <th>Thema</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Thema 1</td>
-          </tr>
-        </v-simple-table>
-        <v-btn
-            class="ma-2"
-            color="primary"
-            dark
+          Toggle
+        </v-btn>-->
+        <v-navigation-drawer
+            v-model="drawer"
+            permanent
         >
-          Accept
-          <v-icon
-              dark
-              right
-          >
-            mdi-checkbox-marked-circle
-          </v-icon>
-        </v-btn>
+          <v-checkbox v-for="bucket in buckets" :key="bucket.id" v-model="selected" :value="bucket.id"
+                      :label=bucket.name @change="sortSelectedBuckets">
+          </v-checkbox>
+        </v-navigation-drawer>
       </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="d-flex justify-center">
-        <v-simple-table v-for="selectedBuckedId in selected" :key="selectedBuckedId.id" class="mr-4" style="width: 30%">
-          <tr>
-            <th colspan="2">
-              {{ buckets.find(bucket => bucket.id === selectedBuckedId).name }}
-            </th>
-          </tr>
-          <tr v-for="ticket in tickets.filter(ticket => ticket.bucket===buckets.find(bucket => bucket.id === selectedBuckedId).name)" :key="ticket.id">
-            <td>{{ticket.id}}</td>
-            <td>{{ticket.thema}}</td>
-          </tr>
-        </v-simple-table>
+      <v-col cols="9" class="d-flex flex-column">
+        <v-row justify="center">
+          <v-btn
+              class="ma-2"
+              color="primary"
+              dark
+          >
+            Accept
+            <v-icon
+                dark
+                right
+            >
+              mdi-checkbox-marked-circle
+            </v-icon>
+          </v-btn>
+          <v-simple-table style="width: 30%;">
+            <tr>
+              <th>ID</th>
+              <th>Thema</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Thema 1</td>
+            </tr>
+          </v-simple-table>
+          <v-btn
+              class="ma-2"
+              color="primary"
+              dark
+          >
+            Accept
+            <v-icon
+                dark
+                right
+            >
+              mdi-checkbox-marked-circle
+            </v-icon>
+          </v-btn>
+        </v-row>
+        <v-row class="d-flex flex-nowrap">
+          <v-simple-table v-for="selectedBuckedId in selected" :key="selected.indexOf(selectedBuckedId)" class="mr-4"
+                          style="width: 30%">
+            <tr>
+              <th colspan="2">
+                {{ buckets.find(bucket => bucket.id === selectedBuckedId).name }}
+              </th>
+            </tr>
+            <tr v-for="ticket in tickets.filter(ticket => ticket.bucket===buckets.find(bucket => bucket.id === selectedBuckedId).name)"
+                :key="ticket.id">
+              <td>{{ ticket.id }}</td>
+              <td>{{ ticket.thema }}</td>
+            </tr>
+          </v-simple-table>
+        </v-row>
       </v-col>
       <v-col cols="2">
-        <v-simple-table>
-          <tr>
-            <th>ID</th>
-            <th>Thema</th>
-          </tr>
-          <tr v-for="ticket in tickets" :key="ticket.id">
-            <td>{{ticket.id}}</td>
-            <td>{{ticket.thema}}</td>
-          </tr>
-        </v-simple-table>
+<!--          <v-btn
+              color="pink"
+              dark
+              @click.stop="drawer2 = !drawer2"
+          >
+            Toggle
+          </v-btn>-->
+        <v-navigation-drawer
+            v-model="drawer2"
+            permanent
+            right
+        >
+          <v-simple-table>
+            <tr>
+              <th>ID</th>
+              <th>Thema</th>
+            </tr>
+            <tr v-for="ticket in tickets" :key="ticket.id">
+              <td>{{ ticket.id }}</td>
+              <td>{{ ticket.thema }}</td>
+            </tr>
+          </v-simple-table>
+        </v-navigation-drawer>
       </v-col>
     </v-row>
   </v-container>
@@ -82,9 +107,16 @@ export default {
   data() {
     return {
       checked: true,
-      selected: [],
+      drawer: null,
+      drawer2: null,
+      mini: true,
+      selected: [0, 1, 2, 3, 4],
       buckets: [{id: 0, name: 'XS'}, {id: 1, name: 'S'}, {id: 2, name: 'M'}, {id: 3, name: 'L'}, {id: 4, name: 'XL'}],
-      tickets: [{id: 0, thema: 'UI erstellen', bucket:'S'}, {id: 1, thema: 'Suchfunktion', bucket:'XS'}, {id: 2, thema: 'Migration', bucket:'M'}]
+      tickets: [{id: 0, thema: 'UI erstellen', bucket: 'S'}, {id: 1, thema: 'Suchfunktion', bucket: 'XS'}, {
+        id: 2,
+        thema: 'Migration',
+        bucket: 'M'
+      }]
     }
   },
   methods: {
