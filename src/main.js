2598ed31629f3966ed00ec1689f87791545099be
Daniel Ackermann <d.ackermann@smf.de>
2023-02-07 14:45:46 +0100

    store zur main.js hinzugefügt

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import store from './store'
 import VueRouter from "vue-router";
 
 Vue.config.productionTip = false
+
 Vue.use(VueRouter)
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
