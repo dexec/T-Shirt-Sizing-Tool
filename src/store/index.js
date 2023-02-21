b8826abe7004df5ae6b53e00ee052e71d8a0d82e
Daniel Ackermann <d.ackermann@smf.de>
2023-02-21 10:59:18 +0100

    Getter um die Baumstruktur in eine Liste zu überführen

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import saveFile from './file.json'
+import Paket from '../Paket.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
         buckets: saveFile.buckets,
-        packete: saveFile.packete,
+        pakete: saveFile.pakete,
         testBuckets: [
             {
                 id: 0,
@@ -19,7 +20,26 @@ export default new Vuex.Store({
             }
         ]
     },
-    getters: {},
+    getters: {
+        asList(state) {
+            const result = [];
+            const stack = [];
+            for (let paket of state.pakete) {
+                stack.push(paket)
+            }
+            while (stack.length > 0) {
+                const aktuellesPaket = stack.pop();
+                const neuesPaket = new Paket(aktuellesPaket.id, aktuellesPaket.ticket_nr, aktuellesPaket.thema, aktuellesPaket.beschreibung, aktuellesPaket.komponente, aktuellesPaket.bucket, aktuellesPaket.schaetzung, aktuellesPaket.children)
+                if (aktuellesPaket.children.length > 0) {
+                    for (let paketChild of aktuellesPaket.children)
+                        stack.push(paketChild)
+                }
+                result.push(neuesPaket)
+            }
+            console.log("debug")
+            return result
+        }
+    },
     mutations: {
         updateAllBuckets(state, newBucketArray) {
             state.buckets = newBucketArray
